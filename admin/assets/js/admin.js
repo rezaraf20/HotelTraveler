52bb03f6f6aae46351099e3dd7ad3b0aab03cbaa
Reza Rafiei <86382022+rezaraf20@users.noreply.github.com>
2025-10-12 15:28:09 +0330

    Create admin.js

---
+++ admin/assets/js/admin.js
@@ -0,0 +1,7 @@
+/* admin/assets/js/admin.js */
+jQuery(document).ready(function($) {
+    console.log('Ratehawk Admin loaded');
+    
+    // Toggle API key visibility
+    $('#api_key').attr('type', 'text');
+});
